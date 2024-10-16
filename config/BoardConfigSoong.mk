2c023c1376502925c1679023a85bcb7910c043b0
hurtcopain <jacksummers715@gmail.com>
2024-10-16 15:29:55 -0400

    nuke soong header generator

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,19 +1,3 @@
-ifneq (,$(wildcard $(OUT_DIR)/.path_interposer_origpath))
-ORIG_PATH := $(shell cat $(OUT_DIR)/.path_interposer_origpath)
-endif
-# Add variables that we wish to make available to soong here.
-EXPORT_TO_SOONG := \
-    ORIG_PATH \
-    KERNEL_ARCH \
-    KERNEL_BUILD_OUT_PREFIX \
-    KERNEL_CROSS_COMPILE \
-    KERNEL_MAKE_CMD \
-    KERNEL_MAKE_FLAGS \
-    PATH_OVERRIDE_SOONG \
-    TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_SOURCE \
-    TARGET_KERNEL_CLANG_PATH
-
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
